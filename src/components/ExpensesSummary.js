d0ba92e6c74ff835ad7563439e4bda7c8e278a0d
Michael Henry <michaeljhenry@hotmail.ca>
2020-03-02 02:29:45 -0500

    Add expense summary to dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,26 +4,50 @@ import numeral from 'numeral';
 import visibleExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 import moment from 'moment'
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
 
-export class ExpensesSummary extends React.Component {
-    render() {
-        return(
-            <div>
-                <p>{expensesTotal(this.props.expenses)}</p>
-                <p>{visibleExpenses(this.props.expenses, this.props.filters)}</p>
-            </div>
-        );
-    };
+export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
+    return(
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+        </div>
+    )
 };
 
-const mapStateToProps = (state,props) => ({
-    expenses: state.expenses,
-    filters: state.filters
-});
+const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    return {
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
+    }
+};
 
-const mapDispatchToProps = (dispatch) => ({
-    visibleExpenses: (expenses, filters) => dispatch(visibleExpenses(expenses, filters)),
-    expensesTotal: (expenses) => dispatch(expensesTotal(expenses))
-});
-export default connect(mapStateToProps,mapDispatchToProps)(ExpensesSummary);
+export default connect(mapStateToProps)(ExpensesSummary);
+
+// MY ATTEMPT:  LECTURE 140
+
+// export class ExpensesSummary extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <p>{expensesTotal(this.props.expenses)}</p>
+//                 <p>{visibleExpenses(this.props.expenses, this.props.filters)}</p>
+//             </div>
+//         );
+//     };
+// };
+
+// const mapStateToProps = (state,props) => ({
+//     expenses: state.expenses,
+//     filters: state.filters
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//     visibleExpenses: (expenses, filters) => dispatch(visibleExpenses(expenses, filters)),
+//     expensesTotal: (expenses) => dispatch(expensesTotal(expenses))
+// });
+// export default connect(mapStateToProps,mapDispatchToProps)(ExpensesSummary);
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -5,22 +5,42 @@ import expenses from '../fixtures/expenses';
 import {filters, altFilters} from '../fixtures/filters';
 import moment from 'moment';
 
-let visibleExpenses, expensesTotal, wrapper;
 
-beforeEach(() => {
-    visibleExpenses = jest.fn();
-    expensesTotal = jest.fn();
 
-    wrapper = shallow(
-        <ExpensesSummary
-            visibleExpenses={visibleExpenses}
-            expensesTotal = {expensesTotal}
-            expenses = {expenses}
-            filters = {filters}
-        />)
+test('should correctly render ExpensesSummary with 1 expense', () => {
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal = {234}/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should correctly render ExpensesSummary with multiple expenses', () => {
+    const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal = {23579876876}/>);
+    expect(wrapper).toMatchSnapshot();
 });
 
 
-test('should display the correct amount of expenses and correct toal', () => {
-    expect(wrapper).toMatchSnapshot();
-}); 
+
+
+
+
+
+// MY ATTEMPT : LECTURE 140
+
+// let visibleExpenses, expensesTotal, wrapper;
+
+// beforeEach(() => {
+//     visibleExpenses = jest.fn();
+//     expensesTotal = jest.fn();
+
+//     wrapper = shallow(
+//         <ExpensesSummary
+//             visibleExpenses={visibleExpenses}
+//             expensesTotal = {expensesTotal}
+//             expenses = {expenses}
+//             filters = {filters}
+//         />)
+// });
+
+
+// test('should display the correct amount of expenses and correct toal', () => {
+//     expect(wrapper).toMatchSnapshot();
+// }); 
